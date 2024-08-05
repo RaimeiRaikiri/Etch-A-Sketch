8fb983e846eb2cf971c373afdfefebe9da06cce2
RaimeiRaikiri <149266085+Raikiri45@users.noreply.github.com>
2024-08-06 00:06:10 +0100

    changed reset function so it reinstates the grid after wiping it

--- javascript.js
+++ javascript.js
@@ -5,26 +5,39 @@ const setGridButton = document.querySelector('#setGrid');
 const resetGridButton = document.querySelector('#resetGrid');
 
 let containerFull = false;
+let rowLength = 10;
 
 setGridButton.addEventListener('click', function(){
-    const rowLength = prompt('Enter rowLength (max 100)', 10);
+    // prompting outside the function makes the function more scalable for different inputs
+    rowLength = prompt('Enter rowLength (max 100)', 10);
     setGrid(rowLength);
+
 });
 
-resetGridButton.addEventListener('click', ()=>resetGrid(containerFull, gridContainer));
+resetGridButton.addEventListener('click', function(){
+    resetGrid(containerFull, gridContainer);
+    setGrid(rowLength);
+});
 
 function setGrid(size)
 {
     if (size <= 100)
     {
-        fullGridSize = size * size
-        percentage = (1/fullGridSize) * 100;
+        if (containerFull)
+        {
+            resetGrid(containerFull, gridContainer);
+        }
 
+        fullGridSize = size * size
+        // Determines percentage of an individual square so the height and width of each div can be set appropriately
+        percentage = (1/fullGridSize) * 100;
+        sizeOfSingleSquare = percentage * size;
         for (let i = 1; i <= fullGridSize; i++)
         {
             const currentDiv = document.createElement('div');
             currentDiv.classList.add('colorSquare');
-            currentDiv.style.cssText = `border: 1px solid black; width: ${percentage*size}%; height: ${percentage*size}`;
+            currentDiv.style.cssText = `border: 1px solid #FB4F93; width: ${sizeOfSingleSquare}%; height: ${sizeOfSingleSquare}`;
+            currentDiv.addEventListener('mouseover', () => currentDiv.style.backgroundColor = 'pink')
             gridContainer.appendChild(currentDiv);
         }
         containerFull = true;
@@ -34,8 +47,9 @@ function setGrid(size)
 function resetGrid(containerFull, container){
     if (containerFull)
         {
-            while (container.firstChild) {
-                container.removeChild(container.firstChild);
+            while (container.firstChild) 
+                {
+                    container.removeChild(container.firstChild);
                 }
             containerFull = false;
         }
