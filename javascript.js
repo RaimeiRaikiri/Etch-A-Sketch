8e177a269ee0fe433be8808dcf17c4cdef4b42e3
RaimeiRaikiri <149266085+Raikiri45@users.noreply.github.com>
2024-08-05 23:07:55 +0100

    added reset grid button function and event listener, new variable to check if container is full

--- javascript.js
+++ javascript.js
@@ -2,11 +2,17 @@ const gridContainer = document.querySelector('#gridContainer');
 
 const buttonContainer = document.querySelector('#buttonContainer');
 const setGridButton = document.querySelector('#setGrid');
+const resetGridButton = document.querySelector('#resetGrid');
+
+let containerFull = false;
 
 setGridButton.addEventListener('click', function(){
     const rowLength = prompt('Enter rowLength (max 100)', 10);
     setGrid(rowLength);
 });
+
+resetGridButton.addEventListener('click', ()=>resetGrid(containerFull, gridContainer));
+
 function setGrid(size)
 {
     if (size <= 100)
@@ -17,8 +23,20 @@ function setGrid(size)
         for (let i = 1; i <= fullGridSize; i++)
         {
             const currentDiv = document.createElement('div');
+            currentDiv.classList.add('colorSquare');
             currentDiv.style.cssText = `border: 1px solid black; width: ${percentage*size}%; height: ${percentage*size}`;
             gridContainer.appendChild(currentDiv);
         }
+        containerFull = true;
     };
+}
+
+function resetGrid(containerFull, container){
+    if (containerFull)
+        {
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+                }
+            containerFull = false;
+        }
 }
